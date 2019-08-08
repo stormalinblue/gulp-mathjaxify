1e061f55097d57eafb686a7f239b428670102922
Muktaka Joshipura <muktaka2000@gmail.com>
2019-08-08 15:13:21 +0530

    test: Fix tests to actually compare files

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,5 @@
 import { expect } from "chai";
+import { readFileSync } from "fs";
 import { dest, src } from "gulp";
 import { join } from "path";
 import { mathjaxify } from "../lib/index";
@@ -10,32 +11,43 @@ function inputPath(filename: string): string {
   return join(TEST_RESOURCES, "input", filename);
 }
 
+function expectedPath(filename: string): string {
+  return join(TEST_RESOURCES, "expected", filename);
+}
+
+function outputPath(filename: string): string {
+  return join(OUTPUT, filename);
+}
+
+function test(
+  inputFile: string, configOptions: any, typesetOptions: any): void {
+  src(inputPath(inputFile))
+    .pipe(mathjaxify(configOptions, typesetOptions))
+    .pipe(dest(OUTPUT));
+  expect(readFileSync(expectedPath(inputFile)).toString())
+    .to.be.equal(readFileSync(outputPath(inputFile)).toString());
+}
+
 describe("Converting math to <svg> elements", () => {
   it("should create 1 svg with 1 display 1 inline", () => {
-    src(inputPath("svg-1i1d-0i.html"))
-      .pipe(mathjaxify({ input: ["TeX"] }, { svg: true }))
-      .pipe(dest(OUTPUT));
-    expect(1).to.be.equal(1);
+    test("svg-1i1d-0i.html", { input: ["TeX"] }, { svg: true });
   });
   it("should create 2 svg with 1 display 1 inline", () => {
-    src(inputPath("svg-1i1d-1i.html"))
-      .pipe(mathjaxify({ input: ["TeX"], singleDollars: true }, { svg: true }))
-      .pipe(dest(OUTPUT));
-    expect(1).to.be.equal(1);
+    test(
+      "svg-1i1d-1i.html",
+      { input: ["TeX"], singleDollars: true },
+      { svg: true });
   });
 });
 
 describe("Converting math to HTML elements", () => {
   it("should create 1 svg with 1 display 1 inline", () => {
-    src(inputPath("html-1i1d-0i.html"))
-      .pipe(mathjaxify({ input: ["TeX"] }, { html: true }))
-      .pipe(dest(OUTPUT));
-    expect(1).to.be.equal(1);
+    test("html-1i1d-0i.html", { input: ["TeX"] }, { html: true });
   });
   it("should create 2 svg with 1 display 1 inline", () => {
-    src(inputPath("html-1i1d-1i.html"))
-      .pipe(mathjaxify({ input: ["TeX"], singleDollars: true }, { html: true }))
-      .pipe(dest(OUTPUT));
-    expect(1).to.be.equal(1);
+    test(
+      "html-1i1d-1i.html",
+      { input: ["TeX"], singleDollars: true },
+      { html: true });
   });
 });
